40c172b2fa40387378931e5bba8416df1a604adb
Alberto García Hierro <alberto@garciahierro.com>
2014-02-01 17:53:19 +0000

    Move Status to its own file

--- lib.go
+++ lib.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"encoding/binary"
-	"fmt"
-	"gnd.la/log"
 	"io"
-	"os/exec"
-	"sync"
-	"time"
 )
 
 const (
@@ -22,89 +17,6 @@ const (
 	respOk
 	respErr
 )
-
-type State uint8
-
-const (
-	StateStopped State = iota
-	StateStarted
-	StateFailed
-)
-
-type Status struct {
-	sync.Mutex
-	Config   *Config
-	Cmd      *exec.Cmd
-	State    State
-	Started  time.Time
-	Restarts int
-	Err      error
-}
-
-func newStatus(cfg *Config) *Status {
-	return &Status{Config: cfg}
-}
-
-func (s *Status) Run() {
-	s.Lock()
-	s.State = StateStarted
-	s.Unlock()
-	for {
-		s.Lock()
-		name := s.Config.ServiceName()
-		if s.State != StateStarted {
-			s.Unlock()
-			break
-		}
-		cmd, err := s.Config.Cmd()
-		if err != nil {
-			s.State = StateFailed
-			s.Err = err
-			log.Errorf("could not initialize %s: %s", name, err)
-			s.Unlock()
-			break
-		}
-		s.Cmd = cmd
-		s.Started = time.Now()
-		log.Infof("Starting %s", name)
-		if err := s.Cmd.Start(); err != nil {
-			s.State = StateFailed
-			s.Err = err
-			log.Errorf("failed to start %s: %s", name, err)
-			s.Unlock()
-			break
-		}
-		s.Unlock()
-		err = s.Cmd.Wait()
-		s.Lock()
-		if s.State != StateStarted {
-			s.Unlock()
-			break
-		}
-		if since := time.Since(s.Started); since < time.Second {
-			// Consider failure
-			s.State = StateFailed
-			s.Err = fmt.Errorf("exited too fast (%s)", since)
-			s.Unlock()
-			log.Errorf("%s %s", name, s.Err)
-			break
-		}
-		s.Restarts++
-		s.Unlock()
-		log.Infof("%s exited with error %s - restarting", name, err)
-	}
-}
-
-func (s *Status) Stop() {
-	s.Lock()
-	s.State = StateStopped
-	s.Restarts = 0
-	s.Unlock()
-	if s.Cmd != nil && s.Cmd.Process != nil {
-		log.Infof("Stopping %s", s.Config.ServiceName())
-		s.Cmd.Process.Kill()
-	}
-}
 
 func encodeString(w io.Writer, s string) error {
 	length := uint32(len(s))
---
+++ status.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"gnd.la/log"
+	"os/exec"
+	"sync"
+	"time"
+)
+
+type State uint8
+
+const (
+	StateStopped State = iota
+	StateStarted
+	StateFailed
+)
+
+type Status struct {
+	sync.Mutex
+	Config   *Config
+	Cmd      *exec.Cmd
+	State    State
+	Started  time.Time
+	Restarts int
+	Err      error
+}
+
+func newStatus(cfg *Config) *Status {
+	return &Status{Config: cfg}
+}
+
+func (s *Status) Run() {
+	s.Lock()
+	s.State = StateStarted
+	s.Unlock()
+	for {
+		s.Lock()
+		name := s.Config.ServiceName()
+		if s.State != StateStarted {
+			s.Unlock()
+			break
+		}
+		cmd, err := s.Config.Cmd()
+		if err != nil {
+			s.State = StateFailed
+			s.Err = err
+			log.Errorf("could not initialize %s: %s", name, err)
+			s.Unlock()
+			break
+		}
+		s.Cmd = cmd
+		s.Started = time.Now()
+		log.Infof("Starting %s", name)
+		if err := s.Cmd.Start(); err != nil {
+			s.State = StateFailed
+			s.Err = err
+			log.Errorf("failed to start %s: %s", name, err)
+			s.Unlock()
+			break
+		}
+		s.Unlock()
+		err = s.Cmd.Wait()
+		s.Lock()
+		if s.State != StateStarted {
+			s.Unlock()
+			break
+		}
+		if since := time.Since(s.Started); since < time.Second {
+			// Consider failure
+			s.State = StateFailed
+			s.Err = fmt.Errorf("exited too fast (%s)", since)
+			s.Unlock()
+			log.Errorf("%s %s", name, s.Err)
+			break
+		}
+		s.Restarts++
+		s.Unlock()
+		log.Infof("%s exited with error %s - restarting", name, err)
+	}
+}
+
+func (s *Status) Stop() {
+	s.Lock()
+	s.State = StateStopped
+	s.Restarts = 0
+	s.Unlock()
+	if s.Cmd != nil && s.Cmd.Process != nil {
+		log.Infof("Stopping %s", s.Config.ServiceName())
+		s.Cmd.Process.Kill()
+	}
+}
